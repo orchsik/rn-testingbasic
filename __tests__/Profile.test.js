bd0e0c4d637ca343464effc6130f749604567ee2
orchsik <orchsik@naver.com>
2021-12-05 12:24:07 +0900

    Feat: gitignore 추가(coverage/)

--- __tests__/Profile.test.js
+++ __tests__/Profile.test.js
@@ -4,13 +4,13 @@ import {fireEvent, render} from '@testing-library/react-native';
 
 describe('[Profile Test', () => {
   const props = {
-    userName: '액션가면',
-    name: '짱구',
+    userName: '호빵맨',
+    name: '세균맨',
   };
 
-  test('유저이름이 알맞게 나온다.', () => {
+  test('설정한 유저이름이 "호빵맨"이 나온다.', () => {
     const rendered = render(<Profile {...props} />);
-    rendered.getByText(/액션가면/);
+    rendered.getByText(/호빵맨/);
   });
 
   test('버튼이 눌리면 메시지가 적절히 변경된다.', () => {
